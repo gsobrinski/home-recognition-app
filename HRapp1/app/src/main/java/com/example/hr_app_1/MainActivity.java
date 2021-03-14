a546b2413da234bf2bc5e2252be8eb7aac1afd2f
gsobrinski <gsobrinski99@gmail.com>
2021-03-14 15:42:24 -0400

    integrated ML model and created MlActivity to display image and results

--- HRapp1/app/src/main/java/com/example/hr_app_1/MainActivity.java
+++ HRapp1/app/src/main/java/com/example/hr_app_1/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -18,6 +19,7 @@ import androidx.navigation.ui.NavigationUI;
 public class MainActivity extends AppCompatActivity {
 
     static final int CAMERA_PIC_REQUEST = 1337;
+    static final int UPLOAD_PIC_REQUEST = 1338;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +35,16 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+        // upload image button
+        Button uploadButton = findViewById(R.id.uploadButton);
+        uploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent uploadIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(uploadIntent , 1);//one can be replaced with any action code
+            }
+        });
 
         // camera button
         FloatingActionButton fab = findViewById(R.id.fab);
--- HRapp1/app/src/main/java/com/example/hr_app_1/MlActivity.java
+++ HRapp1/app/src/main/java/com/example/hr_app_1/MlActivity.java
@@ -4,10 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Matrix;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class MlActivity extends AppCompatActivity {
+
+    private static final int inputSize = 150;
+    private static final String modelPath = "model.tflite";
+
+
+    private Executor executor = Executors.newSingleThreadExecutor();
+    Classifier classifier;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +35,37 @@ public class MlActivity extends AppCompatActivity {
         setTitle("Model Results");
 
         Intent intent = getIntent();
-        Bitmap image = (Bitmap) intent.getParcelableExtra("BitmapImage");
+        Bitmap modelImage = (Bitmap) intent.getParcelableExtra("BitmapImage");
+        Bitmap displayImage = (Bitmap) intent.getParcelableExtra("BitmapImage");
 
-        ImageView imageview = (ImageView) findViewById(R.id.houseImage);
-        imageview.setImageBitmap(image);
+        // create image for model
+        modelImage = Bitmap.createScaledBitmap(modelImage, inputSize, inputSize, false);
+
+        // create image for display
+        Matrix matrix = new Matrix();
+        matrix.postRotate(90);
+        Bitmap rotatedBitmap = Bitmap.createBitmap(displayImage, 0, 0, displayImage.getWidth(), displayImage.getHeight(), matrix, true);
+
+        // display the image
+        ImageView imageView = (ImageView) findViewById(R.id.houseImage);
+        imageView.setImageBitmap(rotatedBitmap);
+
+        // initialize model
+        try {
+            classifier = new Classifier(getAssets(), modelPath, inputSize);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // get image recognition results
+        float[] results = classifier.recognizeImage(modelImage);
+//        for(int i = 0; i < results.length; i++) {
+//            System.out.println(results[i]);
+//        }
+        // display results
+        TextView secondEmpire = (TextView) findViewById(R.id.secondEmpire);
+        TextView tudorRevival = (TextView) findViewById(R.id.tudorRevival);
+        secondEmpire.setText("Second Empire: " + Float.toString(results[0]));
+        tudorRevival.setText("Tudor Revival: " + Float.toString(results[1]));
     }
 }

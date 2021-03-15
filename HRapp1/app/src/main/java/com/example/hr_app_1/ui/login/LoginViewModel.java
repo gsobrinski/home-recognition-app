28ac753ac0f8f74e3522b5e44c996aaf012fab8f
tuh07806 <38734497+tuh07806@users.noreply.github.com>
2021-03-15 19:44:44 -0400

    Login Page initiated

---
+++ HRapp1/app/src/main/java/com/example/hr_app_1/ui/login/LoginViewModel.java
@@ -0,0 +1,11 @@
+package com.example.hr_app_1.ui.login;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import android.util.Patterns;
+
+public class LoginViewModel  extends ViewModel {
+
+}
